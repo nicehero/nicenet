a7ffbf879c35c327aee135571b05e1ea9220e279
nicehero <85948597@qq.com>
2021-10-05 22:49:40 +0800

    ndkBuild

---
+++ ndkBuild/build.bat
@@ -0,0 +1,3 @@
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%\build\cmake\android.toolchain.cmake" -DANDROID_NDK="%ANDROID_NDK_HOME%" -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="arm64-v8a" -DCMAKE_MAKE_PROGRAM="%ANDROID_NDK_HOME%\prebuilt\windows-x86_64\bin\make.exe" ..
+make
+PAUSE
