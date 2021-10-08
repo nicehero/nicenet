3480bdf541188a9a25cdca6a379cb85c6b435a0a
nicehero <85948597@qq.com>
2021-10-08 14:40:34 +0800

    nicenet.h

---
+++ nicenet.h
@@ -0,0 +1,13 @@
+#ifndef __NICE_NET___
+#define __NICE_NET___
+
+#include "Service.h"
+#include "HttpServer.h"
+#include "Tcp.h"
+#include "Log.h"
+#include "Clock.h"
+#include "Kcp.h"
+#include "CopyablePtr.hpp"
+#include "Common.hpp"
+
+#endif
